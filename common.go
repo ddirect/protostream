8598402fab3ab950e9ac1eb3a1d4022a479b5748
ddirect <62487612+ddirect@users.noreply.github.com>
2021-07-06 00:43:01 +0300

    no readwriter interface

--- common.go
+++ common.go
@@ -3,8 +3,6 @@ package protostream
 import (
 	"hash/crc32"
 	"io"
-
-	"google.golang.org/protobuf/proto"
 )
 
 const maxSize = 0x8000
@@ -12,17 +10,11 @@ const maxSize = 0x8000
 var crcTable = crc32.MakeTable(crc32.Castagnoli)
 var crcCheck = crc32.Checksum([]byte{0, 0, 0, 0}, crcTable)
 
-type ReadWriter interface {
-	ReadStream(func([]byte) error) error
-	ReadMessage(proto.Message) error
-	WriteStream(func([]byte) (int, error)) error
-	WriteMessage(proto.Message) error
-	Flush() error
+type ReadWriter struct {
+	*Reader
+	*Writer
 }
 
 func New(rw io.ReadWriter) ReadWriter {
-	return &struct {
-		*Reader
-		*Writer
-	}{NewReader(rw), NewWriter(rw)}
+	return ReadWriter{NewReader(rw), NewWriter(rw)}
 }
